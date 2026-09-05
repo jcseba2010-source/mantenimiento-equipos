c6bb07c2688ad604693a29aa24fee756cb8fe1a8
Julio Cesar Contreras Vargas <jcseba2010@gmail.com>
2026-09-05 16:28:00 -0500

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = "https://jgqbwhtykofncmugzymb.supabase.co";
+const SUPABASE_URL = "https://jgqbwbtvkofncmugzymb.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_KRH7iFouUBGXHG203fmDqw_zZMnEMMK";
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
